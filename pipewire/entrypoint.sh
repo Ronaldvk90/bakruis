b97028e35a74315f15e16afd97567bcf3a6c11b4
Ronald van Kouwen <ronald@pipewire.bttf.lan>
2024-06-14 11:33:44 +0200

    created a seperate AVAHI container

--- pipewire/entrypoint.sh
+++ pipewire/entrypoint.sh
@@ -3,8 +3,6 @@
 ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
 echo "$TZ" > /etc/timezone
 
-avahi-daemon --daemonize --no-chroot
-
 sed -i "s/\<OUTPUT\>/$OUTPUT/" /etc/pipewire/pipewire.conf
 sed -i "s/\<OUTPUT\>/$OUTPUT/" /etc/pipewire/pipewire-pulse.conf
 
