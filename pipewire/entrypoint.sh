400cc7ff3d784feaeef1665d3c3e881e09459cdb
Ronald van Kouwen <Ronaldvk90@outlook.com>
2024-07-06 11:38:22 +0200

    removed obsolete line in pipewire-pulse.conf

--- pipewire/entrypoint.sh
+++ pipewire/entrypoint.sh
@@ -4,6 +4,5 @@ ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
 echo "$TZ" > /etc/timezone
 
 sed -i "s/\<OUTPUT\>/$OUTPUT/" /etc/pipewire/pipewire.conf
-sed -i "s/\<OUTPUT\>/$OUTPUT/" /etc/pipewire/pipewire-pulse.conf
 
 su -c pipewire bakruis
