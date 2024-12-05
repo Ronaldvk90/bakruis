063d7827544bf2bad55811cdfdaba9672ffe7f36
ronaldvk90 <ronald@bakruis.bttf.lan>
2024-12-05 11:31:05 +0100

    fixed stuttering at start of of playback

--- pulseaudio/entrypoint.sh
+++ pulseaudio/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i "s/\<DEVICE\>/$DEVICE/" /etc/pulse/default.pa.d/default.pa
+sed -i "s/\<DEVICE\>/$DEVICE/" /etc/pulse/default.pa
 su - bakruis -c "pulseaudio --verbose --exit-idle-time=-1 --disallow-exit"
