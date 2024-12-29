aad37b6937233327b3b495d44a12938a0338e466
Ronald van Kouwen <ronald@marty.bttf.lan>
2024-12-29 19:30:58 +0100

    changed audio group assignment. Redirected it to the env file

--- pipewire/entrypoint.sh
+++ pipewire/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+addgroup -g $AUDIO_GROUP pulse_audio && addgroup bakruis pulse_audio && addgroup bakruis audio
 sed -i "s/\<OUTPUT\>/$OUTPUT/" /etc/pipewire/pipewire.conf
 su -c pipewire bakruis
