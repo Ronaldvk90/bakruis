13aeae92c5f5263c6f9fd568e5749e2c60deca91
Ronald van Kouwen <Ronaldvk90@outlook.com>
2025-04-22 13:42:24 +0200

    Removed pipewire completely and merged it in the pipewire-pulse container. Also made every socket a TCP socket

---
+++ pipewire-pulse/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+addgroup -g $AUDIO_GROUP pulse_audio && addgroup bakruis pulse_audio && addgroup bakruis audio
+su -c pipewire bakruis&
+su -c pipewire-pulse bakruis&
+
+while true; do
+    defaultdevice=$(pactl get-default-sink)
+    if [[ "$defaultdevice" == "$DEVICE" ]]; then
+        break
+    else
+        pactl set-default-sink $DEVICE
+        pactl set-sink-volume $DEVICE 100%
+    fi
+    sleep 5
+done &
+
+su -c wireplumber bakruis
--- pipewire/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-addgroup -g $AUDIO_GROUP pulse_audio && addgroup bakruis pulse_audio && addgroup bakruis audio
-su -c pipewire bakruis&
-su -c wireplumber bakruis
