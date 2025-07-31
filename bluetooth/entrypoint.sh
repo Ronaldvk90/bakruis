ff2d53f99c7a744ccfc0bd9dcd11fd471a096ad1
Ronald van Kouwen <ronald@compiler.bttf.lan>
2025-07-31 15:25:18 +0200

    Made bluetooth authentication easyer

--- bluetooth/entrypoint.sh
+++ bluetooth/entrypoint.sh
@@ -3,5 +3,7 @@
 ## Set the advertisement name from the .env file
 sed -i "s/\<NAME\>/$NAME/" /etc/bluetooth/main.conf
 
-## Actual bluetoothd entrypoint
-./usr/lib/bluetooth/bluetoothd
+## Actual bluetoothd Simple agent entrypoint
+/usr/lib/bluetooth/bluetoothd&
+bluetoothctl discoverable yes
+. /root/venv/bin/activate && cd /bluez-5.9/test/ && python simple-agent --capability=NoInputNoOutput
