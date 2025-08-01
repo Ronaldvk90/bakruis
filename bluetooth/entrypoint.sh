47bb8ba2f693bdde6d9f86e52f23fedcd5e79584
Ronald van Kouwen <ronald@compiler.bttf.lan>
2025-08-01 21:39:19 +0200

    Bluetooth doesn't work with different agents (yet)

--- bluetooth/entrypoint.sh
+++ bluetooth/entrypoint.sh
@@ -4,6 +4,4 @@
 sed -i "s/\<NAME\>/$NAME/" /etc/bluetooth/main.conf
 
 ## Actual bluetoothd Simple agent entrypoint
-/usr/lib/bluetooth/bluetoothd&
-bluetoothctl discoverable yes
-. /root/venv/bin/activate && cd /bluez-5.9/test/ && python simple-agent --capability=NoInputNoOutput
+/usr/lib/bluetooth/bluetoothd
