4ee32469cfca639bb2c83bfb51e2bbb1aaf97568
Ronald van Kouwen <ronald@compiler.bttf.lan>
2025-08-03 22:05:48 +0200

    Bluetooth wasn't discoverable OOTB. Now it is.

--- bluetooth/entrypoint.sh
+++ bluetooth/entrypoint.sh
@@ -5,4 +5,24 @@ sed -i "s/\<NAME\>/$NAME/" /etc/bluetooth/main.conf
 
 /usr/lib/bluetooth/bluetoothd --noplugin=keyboard --experimental&
 
+# Bluetooth has te be powered in order to work correctly
+while true; do
+  powered=$(bluetoothctl show | grep Powered | awk '{print $2}')
+  if [ "$powered" = "yes" ]; then
+    break
+  fi
+  echo "waiting for bluetooth controller power..."
+  sleep 1
+done
+
+echo "Bluetooth controller powered on!"
+
+bluetoothctl << EOF
+pairable on
+discoverable on
+discoverable-timeout 0
+EOF
+
+echo "Turned on discovery"
+
 /usr/local/bin/bluetoothaccept
