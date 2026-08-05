6d1e461ca2c4b1c3d421a10bcacc50e401bc9498
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-08-05 15:13:57 +0200

    Simplifing bluetooth part 1 of idk....

--- bluetooth/entrypoint.sh
+++ bluetooth/entrypoint.sh
@@ -3,7 +3,5 @@
 ## Set the advertisement name from the .env file
 sed -i "s/\<NAME\>/$NAME/" /etc/bluetooth/main.conf
 
-/usr/lib/bluetooth/bluetoothd --noplugin=keyboard --experimental&
 /usr/sbin/rfkill unblock bluetooth
-
-bluetoothctl
+/usr/lib/bluetooth/bluetoothd --noplugin=keyboard --experimental
