b9242afd9cbeeb457963ccbf5fcf9f153d516e5b
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-08-05 15:47:33 +0200

    rfkill not needed anymore.

--- bluetooth/entrypoint.sh
+++ bluetooth/entrypoint.sh
@@ -3,5 +3,4 @@
 ## Set the advertisement name from the .env file
 sed -i "s/\<NAME\>/$NAME/" /etc/bluetooth/main.conf
 
-/usr/sbin/rfkill unblock bluetooth
 /usr/lib/bluetooth/bluetoothd --noplugin=keyboard --experimental
