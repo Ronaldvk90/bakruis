10a0352eed6ba64a7070d21e7f7f2568cf75cc14
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-08-05 15:07:36 +0200

    Simplified BT container

--- bluetooth/entrypoint.sh
+++ bluetooth/entrypoint.sh
@@ -6,24 +6,4 @@ sed -i "s/\<NAME\>/$NAME/" /etc/bluetooth/main.conf
 /usr/lib/bluetooth/bluetoothd --noplugin=keyboard --experimental&
 /usr/sbin/rfkill unblock bluetooth
 
-# Bluetooth has te be powered in order to work correctly
-while true; do
-  powered=$(bluetoothctl show | grep Powered | awk '{print $2}')
-  if [ "$powered" = "yes" ]; then
-    break
-  fi
-  echo "waiting for bluetooth controller power..."
-  sleep 1
-done
-
-echo "Bluetooth controller powered on!"
-
-bluetoothctl << EOF
-pairable on
-discoverable on
-discoverable-timeout 0
-EOF
-
-echo "Turned on discovery"
-
-/usr/local/bin/bluetoothaccept
+bluetoothctl
