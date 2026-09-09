3eaf44128d8464f78f3f29bf82724cd5be9fde92
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-09-09 19:58:41 +0200

    I think i can manage without a seperate sinkvolume script. Start testing!

--- pulseaudio/entrypoint.sh
+++ pulseaudio/entrypoint.sh
@@ -4,16 +4,19 @@
 addgroup -g $AUDIO_GROUP pulse_audio && addgroup bakruis pulse_audio && addgroup bakruis audio && addgroup pulse bakruis_audio
 
 
-while true; do
-    defaultdevice=$(pactl get-default-sink)
-    if [[ "$defaultdevice" = "$DEVICE" ]]; then
-        /sinkvolume.sh
-        break
-    else
-        pactl set-default-sink $DEVICE
-    fi
-    sleep 5
-done &
+# while true; do
+#     defaultdevice=$(pactl get-default-sink)
+#     if [[ "$defaultdevice" = "$DEVICE" ]]; then
+#         /sinkvolume.sh
+#         break
+#     else
+#         pactl set-default-sink $DEVICE
+#     fi
+#     sleep 5
+# done &
+for SINK in $(pactl list short sinks | awk '{print $1}'); do
+  pactl set-sink-volume "$SINK" 100%
+done
 
 ## Start pulseaudio
 #runuser -l  bakruis -c 'pulseaudio -v --exit-idle-time=-1 --disallow-exit=yes'
