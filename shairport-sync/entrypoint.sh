2902d8b6000f6b2f203e146f4bd22a97e771c1eb
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-09-09 21:58:41 +0200

    entrypoint is now at / instead of ./

--- shairport-sync/entrypoint.sh
+++ shairport-sync/entrypoint.sh
@@ -3,4 +3,5 @@ set -e
 
 addgroup -g $AUDIO_GROUP shairport_audio && addgroup bakruis shairport_audio && addgroup bakruis audio && addgroup pulse shairport_audio
 
+/usr/local/bin/nqptp > /dev/null 2>&1
 exec /usr/local/bin/shairport-sync "$@"
